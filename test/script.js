b55d289cfa61e808d291cf5498b902277bd0ddf1
kienerxyzzy <kn242158@gmail.com>
2025-11-19 13:08:49 +0700

    fix submit button

--- test/script.js
+++ test/script.js
@@ -1,4 +1,4 @@
-const enableAI = true;
+const enableAI = false;
 
 var selected = localStorage.getItem("theme");
 selected = selected ?? "light";
@@ -68,12 +68,10 @@ ActionButton.addEventListener("click", () => {
     stage = 1;
     ActionButton.innerHTML = "Submit";
     TestBox.className = "";
-    startTimer(60 * 60);//can and will be changed later
-    enableTest(); 
+    startTimer(60 * 60); //can and will be changed later
+    enableTest();
   } else {
-    clearInterval(intervalId);
-    TimerElement.style = /*css*/ "color:var(--accent-color)";
-    window.confirm('Are you sure you want to submit?') && submit();
+    window.confirm("Are you sure you want to submit?") && submit();
   }
 });
 
@@ -90,7 +88,7 @@ function disableTest() {
 disableTest();
 
 function enableTest() {
-  document.getElementById('test').className = '';
+  document.getElementById("test").className = "";
   TestBox.querySelectorAll("input, textarea").forEach((e) => {
     e.disabled = false;
   });
@@ -98,8 +96,6 @@ function enableTest() {
 
 function submit() {
   stage = 2;
-  //tbh idk what should be done here
-  //leave that for qh
   TimerElement.style = /*css*/ "color:var(--accent-color)";
   clearInterval(intervalId);
   console.log("Test is done!");
@@ -137,37 +133,39 @@ async function getAIResponse(prompt = "") {
 // CODE STARTS HERE
 // is hpol or aqme a better username/displayname (pls answer i need to pick)
 
-let questionsLeftToGenerate = 0
+let questionsLeftToGenerate = 0;
 function setGenerationFinished(questions) {
   questionsLeftToGenerate = questions;
 }
 
 function checkGenerationFinished() {
   questionsLeftToGenerate -= 1;
-  (questionsLeftToGenerate <= 0) && readyTest();
+  questionsLeftToGenerate <= 0 && readyTest();
 }
 
-document.getElementById('title').innerText = `${translationKeys[testType].toUpperCase()} PRACTICE TEST`;
-if (testType != 'toeic') {
-  if (testIncludes.includes('writing')) {
+document.getElementById("title").innerText = `${translationKeys[
+  testType
+].toUpperCase()} PRACTICE TEST`;
+if (testType != "toeic") {
+  if (testIncludes.includes("writing")) {
     setGenerationFinished(1);
-    const section1 = document.createElement('div');
-    const section1Title = document.createElement('h3');
-    const section1Question = document.createElement('p');
-    const section1Textbox = document.createElement('textarea');
-    
-    section1.className = 'section';
-    section1Title.innerText = 'Writing task 1';
-		getAIResponse(
-		  `I'm practicing for ${translationKeys[testType]}, can you generate a writing part 1 question for me? I don't want any tips/directions, as I'd like this to be a sort of mock test. \nNotes: you should ONLY ANSWER WITH THE EXAM QUESTION and nothing else, not even a title like 'IELTS Writing part 2'; and also not use any markdown, just plain text. Thanks.`
-		).then(response => {
-      section1Question.innerText = response;
+    const section1 = document.createElement("div");
+    const section1Title = document.createElement("h3");
+    const section1Question = document.createElement("p");
+    const section1Textbox = document.createElement("textarea");
+
+    section1.className = "section";
+    section1Title.innerText = "Writing task 1";
+    getAIResponse(
+      `I'm practicing for ${translationKeys[testType]}, can you generate a writing part 1 question for me? I don't want any tips/directions, as I'd like this to be a sort of mock test. \nNotes: you should ONLY ANSWER WITH THE EXAM QUESTION and nothing else, not even a title like 'IELTS Writing part 2'; Thanks.`
+    ).then((response) => {
+      section1Question.innerHTML = marked.parse(response);
       checkGenerationFinished();
-    })
-    
+    });
+
     section1.appendChild(section1Title);
     section1.appendChild(section1Question);
     section1.appendChild(section1Textbox);
-    document.getElementById('test').appendChild(section1);
+    document.getElementById("test").appendChild(section1);
   }
 }
